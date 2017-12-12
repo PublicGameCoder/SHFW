fb00f08883bdbdd11d00a8378a3ac6918cd480e4
PublicGameCoder <mikedegroot1999@hotmail.com>
2017-12-12 09:42:15 +0100

    add Camera

---
+++ SHFW/Camera.h
@@ -0,0 +1,14 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+
+void computeMatricesFromInputs(GLFWwindow* window);
+glm::mat4 getViewMatrix();
+glm::vec3 getCursor();
+
+#endif
