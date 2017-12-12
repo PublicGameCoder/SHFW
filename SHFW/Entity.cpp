fea42fd1c2917bd31ca855f12f860ff29003a4b4
PublicGameCoder <mikedegroot1999@hotmail.com>
2017-12-12 09:57:06 +0100

    add Input,children deconstruction,update and includes

--- SHFW/Entity.cpp
+++ SHFW/Entity.cpp
@@ -1,9 +1,19 @@
 #include "Entity.h"
 
 Entity::Entity() {
-
+	this->_input = InputManager::getManager();
 }
 
 Entity::~Entity() {
+	for each (Entity* child in childs) {
+		delete child;
+	}
+}
 
+void Entity::update() {
+
+}
+
+void Entity::addSprite(std::string file_path) {
+	this->sprite = new Sprite(file_path);
 }
--- SHFW/Entity.h
+++ SHFW/Entity.h
@@ -1,14 +1,27 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include <iostream>
+#include <vector>
+#include "Sprite.h"
+#include "InputManager.h"
 class Entity
 {
 public:
 	Entity();
 	virtual ~Entity();
-
+	virtual void update();
+	std::vector<Entity*> getChilds() {
+		return this->childs;
+	}
+	Sprite* getSprite() { return this->sprite; }
+	void addSprite(std::string file_path);
 private:
-
+	std::vector<Entity*> childs;
+	InputManager* _input;
+	Sprite* sprite;
+protected:
+	InputManager* input() { return this->_input; };
 };
 
 #endif // !ENTITY_H
