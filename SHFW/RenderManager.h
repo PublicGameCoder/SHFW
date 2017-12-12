6bafd3e2c95f3056510ed0f38840766b06f54a2e
PublicGameCoder <mikedegroot1999@hotmail.com>
2017-12-12 09:59:36 +0100

    add RenderManager

--- SHFW/RenderManager.h
+++ SHFW/RenderManager.h
@@ -1,7 +1,24 @@
 #ifndef RENDERMANAGER_H
 #define RENDERMANAGER_H
 
+#include <glad/glad.h>
 #include <glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/euler_angles.hpp>
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "SHFWConfig.h"
+#include "Scene.h"
+#include "Sprite.h"
+#include "Camera.h"
 
 class RenderManager
 {
@@ -9,9 +26,16 @@ public:
 	RenderManager();
 	virtual ~RenderManager();
 
-	GLFWwindow* getCurrentWindow() { return NULL; }
-private:
+	void render(Scene* scene);
 
+	GLFWwindow* getCurrentWindow() { return currentWindow; }
+
+private:
+	GLFWwindow* currentWindow;
+
+	glm::mat4 _projectionMatrix;
+
+	int init();
 };
 
 #endif // RENDERMANAGER_H
