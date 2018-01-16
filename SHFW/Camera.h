cd1a8ba1cbb358c5d4451604aa94a3fac5567d58
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-01-16 19:02:03 +0100

    Add Camera

--- SHFW/Camera.h
+++ SHFW/Camera.h
@@ -1,14 +1,56 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+// Include GLFW
 #include <glfw3.h>
 
+// Include GLM
 #include <glm/glm.hpp>
+#include <glm/ext.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/euler_angles.hpp>
 
-void computeMatricesFromInputs(GLFWwindow* window);
-glm::mat4 getViewMatrix();
-glm::vec3 getCursor();
+#include "VectorT.h"
+
+class Camera
+{
+public:
+	Camera();
+	virtual ~Camera();
+
+	Point3 position;
+	Point3 rotation;
+
+	void tilt(float amount) { rotation.x += amount; };
+
+	void pan(float amount)	{ rotation.y += amount; };
+
+	void roll(float amount)	{ rotation.z += amount; };
+
+	void dolly(float amount) { position += (Point3(_direction.x, _direction.y, _direction.z) * amount); };
+
+	void track(float amount) { position += (Point3(_right.x, _right.y, _right.z) * amount); };
+
+	void boom(float amount)	{ position += (Point3(_up.x, _up.y, _up.z) * amount); };
+
+	virtual void updateCamera();
+
+	glm::mat4 viewMatrix() { return _viewMatrix; };
+
+	glm::mat4 projectionMatrix() { return _projectionMatrix; };
+
+	void perspective();
+
+	void orthogonal();
+
+private:
+	glm::mat4 _viewMatrix;
+	glm::mat4 _projectionMatrix;
+
+	Point3 _offset;
+
+	glm::vec3 _up;
+	glm::vec3 _right;
+	glm::vec3 _direction;
+};
 
 #endif
