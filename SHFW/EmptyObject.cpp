6f990d2a208e1b5ac5d8a16ad95a662eed84f477
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-01-22 09:52:41 +0100

    Implement scale and rotation changes

--- SHFW/EmptyObject.cpp
+++ SHFW/EmptyObject.cpp
@@ -7,10 +7,17 @@ EmptyObject::EmptyObject() : Entity() {
 EmptyObject::~EmptyObject() {
 
 }
+
 static float size = 0;
 void EmptyObject::update() {
 	size += Time::deltaTime;
-	float scl = Math::map(sin(size),0, 1, 1, 2);
+
+	float scl = Math::map(sin(size), 0, 1, 0.5, 0.8);
 	this->scale.x = scl;
 	this->scale.y = scl;
+	this->scale.z = scl;
+
+	float rotSpeed = Math::map(sin(size),0, 1, -1, 1);
+	this->rotation.x -= rotSpeed * Time::deltaTime;
+	this->rotation.y -= rotSpeed * Time::deltaTime;
 }
