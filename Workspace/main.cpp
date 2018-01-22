dfcf43729fb1e9e00fc523c8d925f5d41bb5aa42
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-01-22 10:13:36 +0100

    Add simple camera movement and 3D cube with texture

--- Workspace/main.cpp
+++ Workspace/main.cpp
@@ -2,22 +2,53 @@
 #include "PreviewScene.h"
 #include "StandardAssets.h"
 #include <iostream>
+#include "TargetTexture.h"
 
 int main()
 {
 	Core core;
 
+	TargetTexture* t = new TargetTexture();
+
 	PreviewScene* scene = new PreviewScene();
 	EmptyObject* obj = new EmptyObject();
-	obj->addSprite("assets/lilypad.tga");
+	//obj->addSprite("assets/default.tga");
+	obj->addSprite("assets/pencils.tga");
 	scene->addChild(obj);
-	
-	obj->position.x += 50;
-	obj->position.y += 50;
+	obj->position.x += 200;
+	obj->position.y += 200;
+
+	obj->scale.x = 0.5;
+	obj->scale.y = 0.5;
+	obj->scale.z = 0.5;
 
 	while (scene->isRunning())
 	{
 		core.run(scene);
+		if (InputManager::getManager()->getKey(Down)) {
+			scene->camera()->position.y -= 200 * Time::deltaTime;
+		}
+		if (InputManager::getManager()->getKey(Up)) {
+			scene->camera()->position.y += 200 * Time::deltaTime;
+		}
+		if (InputManager::getManager()->getKey(Right)) {
+			scene->camera()->position.x += 200 * Time::deltaTime;
+		}
+		if (InputManager::getManager()->getKey(Left)) {
+			scene->camera()->position.x -= 200 * Time::deltaTime;
+		}
+		static double prevMouseX = 0;
+		static double prevMouseY = 0;
+		double mouseX = InputManager::getManager()->getMouseX();
+		double mouseY = InputManager::getManager()->getMouseY();
+		if (InputManager::getManager()->getMouse(1)) {
+			if (prevMouseX != mouseX || prevMouseY != mouseY) {
+				scene->camera()->rotation.y -= (mouseX - prevMouseX) * Time::deltaTime;
+				scene->camera()->rotation.x -= (mouseY - prevMouseY) * Time::deltaTime;
+			}
+		}
+		prevMouseX = mouseX;
+		prevMouseY = mouseY;
 	}
 
 	return 0;
